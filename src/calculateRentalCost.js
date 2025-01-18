6d328df10d79ed790bfca18fbcbdaa079ed89c9d
IonutBalasescu <44617121+IonutBalasescu@users.noreply.github.com>
2025-01-19 01:15:25 +0200

    Update calculateRentalCost.js
    Rental problem implementation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,16 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const dailyRentalCost = 40;
+  let discount = 0;
+  
+  if (days >= 7) {
+    discount = 50;
+  } else if (days >= 3) {
+    discount = 20;
+  }
+
+  return days * dailyRentalCost - discount;
 }
 
 module.exports = calculateRentalCost;
